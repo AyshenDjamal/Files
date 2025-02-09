aaca858e8f949c8322b5b2e76453e534f2198163
AyshenDjamal <ayshen.rasul92@gmail.com>
2025-02-09 18:50:29 +0400

    new

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,9 @@ public class Main {
         Menu();
 
 
-}
-   //Menu Methodu
+    }
+
+    //Menu Methodu
     public static void Menu() {
         System.out.println("1: Hesabınız yoxdur? REGISTER");
         System.out.println("2: Hesabınız var? SIGN IN");
@@ -43,77 +44,77 @@ public class Main {
     }
 
     //Clear Method
-    public static void Clear(){
+    public static void Clear() {
         for (int i = 0; i < 3; i++) {
             System.out.println("*********************************");
         }
     }
 
 
-        //Sign Up Method
-        public static void signUp (Scanner sc){
-            System.out.println("-----------SIGN UP--------------");
-            System.out.print("Enter your name: ");
-            String name = sc.next();
-            System.out.print("Enter your  last name: ");
-            String last_name = sc.next();
-            System.out.print("Enter your email: ");
-            String email = sc.next();
-            System.out.print("Enter your new password: ");
-            String password = sc.next();
-            try {
-                FileWriter writer = new FileWriter("logs.txt");
-                writer.write("SIGN UP-Name: " + name + ", Last Name: " + last_name + ", Email: " + email + ", Password: " + password);
-                System.out.println("------------------------------");
-                System.out.println(name + " "+last_name + ", uğurla qeydiyyatdan keçdiniz!\n");
-                writer.close();
-            } catch (IOException e) {
-                System.err.println("ERROR" + e.getMessage());
-                e.printStackTrace();
-            }
-            Clear();
-            Menu();
-
+    //Sign Up Method
+    public static void signUp(Scanner sc) {
+        System.out.println("-----------SIGN UP--------------");
+        System.out.print("Enter your name: ");
+        String name = sc.next();
+        System.out.print("Enter your  last name: ");
+        String last_name = sc.next();
+        System.out.print("Enter your email: ");
+        String email = sc.next();
+        System.out.print("Enter your new password: ");
+        String password = sc.next();
+        try {
+            FileWriter writer = new FileWriter("logs.txt");
+            writer.write("SIGN UP-Name: " + name + ", Last Name: " + last_name + ", Email: " + email + ", Password: " + password);
+            System.out.println("------------------------------");
+            System.out.println(name + " " + last_name + ", uğurla qeydiyyatdan keçdiniz!\n");
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("ERROR" + e.getMessage());
+            e.printStackTrace();
         }
+        Clear();
+        Menu();
 
-        // Sign In Method
-        public static void signIn (Scanner sc){
-            System.out.println("----------SIGN IN----------");
-            System.out.print("Enter your email: ");
-            String email2 = sc.nextLine();
-            System.out.print("Enter your password: ");
-            String password2 = sc.nextLine();
-            try {
-                FileWriter writer = new FileWriter("logs.txt");
-                writer.write("SIGN IN-EMAIL: " + email2 + ", PASSWORD: " + password2);
-                writer.write("Uğurla sistemə giriş edildi!");
-                System.out.println("------------------------------");
-                System.out.println("Uğurla sistemə giriş edildi!\n");
-                writer.close();
-            } catch (IOException e) {
-                System.err.println("ERROR" + e.getMessage());
-                e.printStackTrace();
-            }
-            Clear();
-            Menu();
-        }
-
-        //Log Out Method
-        public static void logOut (Scanner sc){
-            System.out.println("---------LOG OUT---------------");
-            System.out.println("Çıxış etməyinizə əminsinizmi?");
-            String choice = sc.nextLine();
-            if (choice.equals("Beli")) {
-                System.out.println("****************************");
-                System.out.println("Ugurla çıxış edildi!");
-            } else {
-                System.out.println("Hələ də sistemdəsən!\n");
-                Clear();
-                Menu();
-            }
-
-        }
     }
+
+    // Sign In Method
+    public static void signIn(Scanner sc) {
+        System.out.println("----------SIGN IN----------");
+        System.out.print("Enter your email: ");
+        String email2 = sc.nextLine();
+        System.out.print("Enter your password: ");
+        String password2 = sc.nextLine();
+        try {
+            FileWriter writer = new FileWriter("logs.txt");
+            writer.write("SIGN IN-EMAIL: " + email2 + ", PASSWORD: " + password2);
+            writer.write("Uğurla sistemə giriş edildi!");
+            System.out.println("------------------------------");
+            System.out.println("Uğurla sistemə giriş edildi!\n");
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("ERROR" + e.getMessage());
+            e.printStackTrace();
+        }
+        Clear();
+        Menu();
+    }
+
+    //Log Out Method
+    public static void logOut(Scanner sc) {
+        System.out.println("---------LOG OUT---------------");
+        System.out.println("Çıxış etməyinizə əminsinizmi?");
+        String choice = sc.nextLine();
+        if (choice.equals("Beli")) {
+            System.out.println("****************************");
+            System.out.println("Ugurla çıxış edildi!");
+        } else {
+            System.out.println("Hələ də sistemdəsən!\n");
+            Clear();
+            Menu();
+        }
+
+    }
+}
 
 
         /*try {
